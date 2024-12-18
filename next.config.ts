f593e11f118a79adba9522a5d84918967294fad1
Jean Jesus <38874366+jeanjs@users.noreply.github.com>
2024-12-18 19:04:56 -0300

    refactor: update url-loader configuration to use options object

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,12 @@ const nextConfig: NextConfig = {
     config.resolve.alias["~"] = path.resolve(__dirname);
     config.module.rules.push({
       test: /\.(woff|woff2|eot|ttf|svg)$/,
-      loader: "url-loader?limit=100000",
+      use: {
+        loader: "url-loader",
+        options: {
+          limit: 100000,
+        },
+      },
     });
     return config;
   },
